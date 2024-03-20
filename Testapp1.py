c87782761541f98db6483cab4f6a12675e3b4736
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2024-03-20 20:36:20 +0530

    Update Testapp1.py

--- Testapp1.py
+++ Testapp1.py
@@ -65,7 +65,7 @@ def recommend_talks_with_sentiment(talk_content, comments, data=df):
 
     # Sort by score and display top recommendations
     recommended_talks = data.sort_values(by='score', ascending=False)
-    return recommended_talks['title'].head(10)  # Return only titles
+    return recommended_talks[['title', 'published_date', 'like_count']].head(10)  # Return titles, published dates, and like counts
 
 # Define Streamlit app
 def main():
@@ -76,14 +76,14 @@ def main():
 
     if st.button('Recommend Talks'):
         # Get recommendations
-        recommended_titles = recommend_talks_with_sentiment([talk_content], comments)
+        recommended_talks = recommend_talks_with_sentiment([talk_content], comments)
         
-        # Display recommended titles
+        # Display recommended talks in tiles
         st.subheader('Recommended Talks:')
-        for index, title in enumerate(recommended_titles, start=1):
-            search_query = title.replace(' ', '+')
+        for index, row in recommended_talks.iterrows():
+            search_query = row['title'].replace(' ', '+')
             google_link = f"https://www.google.com/search?q={search_query}"
-            st.write(f"{index}) {title} - [Go]({google_link})", unsafe_allow_html=True)
+            st.write(f"**{index+1}) {row['title']}** - [{row['publushed_date']} - {row['like_count']} Likes]({google_link})")
 
 if __name__ == '__main__':
     main()
